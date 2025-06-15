bfffbb36e83f8cb118442f1668f884518d7b6b3e
bgc-sadiya <sultanasadiya6@gmail.com>
2025-06-15 20:56:56 +0600

    JS Conditional

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let rahim;
+let karim;
+let rapiq;
+
+rahim = "bike";
+karim = "cycle"; 
+rapiq = "car";
+
+if (rahim =="cyle"){
+    console.log('i will go to market');
+}
+else if (karim =="bike"){
+    console.log('i will go to playground');
+}
+
+else if (rapiq =="cycle"){
+    console.log('i will go to car');
+}
+
+else
+{
+   console.log ('I will go to sleep');
+}
